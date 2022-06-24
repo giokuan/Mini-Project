ea422e3655ac7cbc0f84fbd9800d4dbd6582f536
giokuan <giokuan@gmail.com>
2022-06-24 18:33:44 +0800

    added javascript and html page

---
+++ asset/script.js
@@ -0,0 +1,34 @@
+
+function openNav() {
+    document.querySelector("#mySidebar").style.width = "240px";
+    document.querySelector("#main").style.marginLeft = "240px";
+
+   
+    
+  }
+  
+  function closeNav() {
+    document.querySelector("#mySidebar").style.width = "0";
+    document.querySelector("#main").style.marginLeft= "0";
+    d
+   
+    
+  }
+
+ 
+  
+  
+  //   const btn = document.querySelector('.homebtn');
+
+  //   btn.addEventListener('click', () => {
+
+  //   // btn.style.display = 'show';
+  //   btn.onclick = index.html;
+
+  // })
+
+  
+
+  function homebtn() {
+    document.querySelector('.homebtn').onclick=index.html;
+  }
